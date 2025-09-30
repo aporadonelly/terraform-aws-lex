2285d7f3b8a7316c9847496a5364aaab92ab6018
Nelly Rose Aporado <aporadonelly@gmail.com>
2025-10-01 00:03:57 +0200

    Correctd Lex V2 intent by replacing sample_utterances with sample_utterance blocks

--- modules/lex_bot/main.tf
+++ modules/lex_bot/main.tf
@@ -29,3 +29,46 @@ resource "aws_iam_role" "lex_service_role" {
     ]
   })
 }
+
+# Attach AmazonLexFullAccess
+resource "aws_iam_role_policy_attachment" "lex_full_access" {
+  role       = aws_iam_role.lex_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonLexFullAccess"
+}
+
+# Attach CloudWatch logging
+resource "aws_iam_role_policy_attachment" "cw_logs_access" {
+  role       = aws_iam_role.lex_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+# Locale (English)
+resource "aws_lexv2models_bot_locale" "en_us" {
+  bot_id      = aws_lexv2models_bot.getko_food_bot.id
+  bot_version = "DRAFT"
+  locale_id   = "en_US"
+
+  n_lu_intent_confidence_threshold = 0.70
+}
+
+# Intent (OrderFood)
+resource "aws_lexv2models_intent" "order_food" {
+  bot_id      = aws_lexv2models_bot.getko_food_bot.id
+  bot_version = aws_lexv2models_bot_locale.en_us.bot_version
+  locale_id   = aws_lexv2models_bot_locale.en_us.locale_id
+  name        = "OrderFood"
+
+  sample_utterance {
+    utterance = "I want to order {FoodType}"
+  }
+  sample_utterance {
+    utterance = "Please deliver {FoodType}"
+  }
+  sample_utterance {
+    utterance = "Can I get {Quantity} {FoodType}"
+  }
+
+  fulfillment_code_hook {
+    enabled = false
+  }
+}
