d013d74d63e174a31b6d7ebc514fd999a8275d6b
Nelly Rose Aporado <aporadonelly@gmail.com>
2025-10-01 00:07:13 +0200

    Fixed outputs.tf to match lex v2 model

--- modules/lex_bot/outputs.tf
+++ modules/lex_bot/outputs.tf
@@ -1,11 +1,19 @@
 output "bot_name" {
-  value = aws_lex_bot.getko_food_bot.name
+  value = aws_lexv2models_bot.getko_food_bot.name
+}
+
+output "bot_id" {
+  value = aws_lexv2models_bot.getko_food_bot.id
 }
 
 output "bot_arn" {
-  value = aws_lex_bot.getko_food_bot.arn
+  value = aws_lexv2models_bot.getko_food_bot.arn
+}
+
+output "locale_id" {
+  value = aws_lexv2models_bot_locale.en_us.locale_id
 }
 
 output "intent_name" {
-  value = aws_lex_intent.order_food.name
+  value = aws_lexv2models_intent.order_food.name
 }
